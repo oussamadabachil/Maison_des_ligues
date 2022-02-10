9ad8e29299702c4f13821d92f61bddde661d2782
oussama <oussamadabachilpro@gmail.com>
2022-02-10 17:34:41 +0100

    first push

---
+++ js/app.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",()=>{
+let span_swicth_js = document.querySelector(".span_swicth")
+
+
+span_swicth_js.addEventListener("click",()=>{
+    span_swicth_js.classList.toggle("span_swicth_right")
+})
+
+
+
+
+})
